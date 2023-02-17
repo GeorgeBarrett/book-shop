ec836a7174d4a876245bc10f297686222d195764
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-17 18:37:29 +0000

    useState and useEffect used to fetch data from database via express

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+                const res = await axios.get("http://localhost:3000/books")
+                console.log(res)   
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchAllBooks()
+
+    }, [])
+
+    return (
+        <div>Books</div>
+    )
 }
 
 export default Books

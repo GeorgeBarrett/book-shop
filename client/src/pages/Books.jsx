e3533c80e878747a4db73c3a3e6b9ab8b508abc0
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-20 16:25:46 +0000

    basic styling done on home page aka /books

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -9,12 +9,12 @@ const Books = () => {
     useEffect(() => {
         const fetchAllBooks = async () => {
             try {
-                const res = await axios.get("http://localhost:3000/books")
+                const res = await axios.get("http://localhost:3001/books")
                 setBooks(res.data);   
             } catch (err) {
                 console.log(err);
             }
-        }
+        };
 
         fetchAllBooks();
 
@@ -30,10 +30,12 @@ const Books = () => {
                         <h2>{book.title}</h2>
                         <p>{book.desc}</p>
                         <span>{book.price}</span>
+                        <button className="delete">Delete</button>
+                        <button className="update">Update</button>
                     </div>
                 ))}
             </div>
-            <button><Link to="/add">Add new book</Link></button>
+            <button className="add"><Link to="/add">Add new book</Link></button>
         </div>
     )
 }

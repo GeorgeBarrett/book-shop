3498ad79cc26879440c3c2b53c44911fe57a81a3
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-20 17:42:42 +0000

    nodejs/mysql delete operation complete

--- backend/index.js
+++ backend/index.js
@@ -9,37 +9,48 @@ const db = mysql.createConnection({
     user:"root",
     password: "Terminator2",
     database: "test"
-})
+});
 
 app.use(express.json())
 app.use(cors())
 
 app.get("/", (req, res) => {
     res.json("welcome to the backend")
-})
+});
 
 app.get("/books", (req, res) => {
     const q = "SELECT * FROM books"
     db.query(q, (err,data) => {
         if (err) return res.json(err)
         return res.json(data)
-    })
-})
+    });
+});
 
 app.post("/books", (req, res) => {
-    const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
+    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`,) VALUES (?)"
     const values = [
         req.body.title,
         req.body.desc,
-        req.body.cover
+        req.body.price,
+        req.body.cover,
     ];
 
     db.query(q, [values], (err, data) => {
         if (err) return res.json(err)
         return res.json("Book has been added successfully.")
-    })
-})
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "DELETE FROM books WHERE id = ?"
+
+    db.query(q,[bookId], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Book has been deleted successfully.")
+    });
+});
 
 app.listen(3000, () => {
     console.log("connected to backend")
-})
+});

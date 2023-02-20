bbc85dd45e9f2cef3e0323432becb5f4b50a24ba
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-20 19:31:16 +0000

    code tidied. bug fix - missing a bracket to the right of 'book' inside the map function. more string literals used for cleaner code

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -9,7 +9,7 @@ const Books = () => {
     useEffect(() => {
         const fetchAllBooks = async () => {
             try {
-                const res = await axios.get("http://localhost:3001/books/")
+                const res = await axios.get("http://localhost:8800/books/")
                 setBooks(res.data);   
             } catch (err) {
                 console.log(err);
@@ -18,35 +18,45 @@ const Books = () => {
 
         fetchAllBooks();
 
-    }, [])
+    }, []);
 
     const handleDelete = async (id) => {
         try {
-            await axios.delete("http://localhost:3001/books/"+id)
-            window.location.reload()
+            await axios.delete(`http://localhost:8800/books/${id}`);
+            window.location.reload();
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-    }
+    };
 
     return (
         <div>
             <h1>George's Book Shop</h1>
             <div className="books">
-                {books.map(book => (
+                {books.map((book) => (
                     <div className="book" key={book.id}>
-                        {book.cover && <img src={book.cover} alt={book.title} />}
+                        <img src={book.cover} alt={book.title} />
                         <h2>{book.title}</h2>
                         <p>{book.desc}</p>
                         <span>{book.price}</span>
                         <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
-                        <button className="update"><Link to={`/update/${book.id}`}>Update</Link></button>
+                        <button className="update">
+                            <Link 
+                                to={`/update/${book.id}`}
+                                style={{color: "inherit", textDecoration: "none"}}                            
+                                >Update
+                            </Link>
+                        </button>
                     </div>
                 ))}
             </div>
-            <button className="add"><Link to="/add">Add new book</Link></button>
+            <button className="addHome">
+                <Link to="/add" style={{ color: "inherit", textDecoration: "none"}}>
+                    Add new book
+                </Link>
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Books;

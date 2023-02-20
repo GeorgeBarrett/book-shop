aa324334483fe9f4685f7cb43b07613dc396c304
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-20 13:53:03 +0000

    initial html made for Books.jsx. Data from the books table on mysql workbench can now be fetched and populate Books.jsx

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Books = () => {
 
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
 
     useEffect(() => {
         const fetchAllBooks = async () => {
             try {
                 const res = await axios.get("http://localhost:3000/books")
-                console.log(res)   
+                setBooks(res.data);   
             } catch (err) {
-                console.log(err)
+                console.log(err);
             }
         }
 
-        fetchAllBooks()
+        fetchAllBooks();
 
     }, [])
 
     return (
-        <div>Books</div>
+        <div>
+            <h1>George's Book Shop</h1>
+            <div className="books">
+                {books.map(book => (
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover} alt={book.title} />}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+            <button><Link to="/add">Add new book</Link></button>
+        </div>
     )
 }
 
-export default Books
+export default Books;

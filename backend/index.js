0764efbf17b5448659b050a708816a8f98a57f3f
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-20 19:04:24 +0000

    routing does not seem to be working correctly so i am carrying out some bug finding. I was not returning the books data in some of the db.query sections

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`,) VALUES (?)"
+    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
     const values = [
         req.body.title,
         req.body.desc,
@@ -36,18 +36,18 @@ app.post("/books", (req, res) => {
     ];
 
     db.query(q, [values], (err, data) => {
-        if (err) return res.json(err)
-        return res.json("Book has been added successfully.")
+        if (err) return res.send(err)
+        return res.json(data)
     });
 });
 
 app.delete("/books/:id", (req, res) => {
     const bookId = req.params.id;
-    const q = "DELETE FROM books WHERE id = ?"
+    const q = "DELETE FROM books WHERE id = ?";
 
     db.query(q,[bookId], (err, data) => {
-        if (err) return res.json(err)
-        return res.json("Book has been deleted successfully.")
+        if (err) return res.send(err);
+        return res.json(data)
     });
 });
 
@@ -60,14 +60,14 @@ app.put("/books/:id", (req, res) => {
         req.body.desc,
         req.body.price,
         req.body.cover,
-    ]
+    ];
 
-    db.query(q,[...values, bookId], (err, data) => {
-        if (err) return res.json(err)
-        return res.json("Book has been updated successfully.")
+    db.query(q, [...values, bookId], (err, data) => {
+        if (err) return res.send(err)
+        return res.json(data);
     });
 });
 
-app.listen(3000, () => {
+app.listen(8800, () => {
     console.log("connected to backend")
 });
